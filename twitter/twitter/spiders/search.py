6b94c0876b68bd0043b25cc23f0f4fb8f97eecdf
myj <jmy1330@gmail.com>
2020-06-04 15:50:43 +0900

    twitter 본문 링크 추출가능한 상태 429 error code 처리 필요함

--- twitter/twitter/spiders/search.py
+++ twitter/twitter/spiders/search.py
@@ -4,6 +4,7 @@ import pprint
 import scrapy
 import orjson
 import json
+import re
 
 
 def get_params(query: str, cursor=None) -> str:
@@ -27,12 +28,14 @@ class SearchSpider(scrapy.Spider):
             'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
         },
         'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36',
-        'DOWNLOAD_DELAY': 3,
+        'DOWNLOAD_DELAY': 0,
         'WAIT_FOR_SCALE': 1
     }
-    handle_httpstatus_list = [400, 404]
+    handle_httpstatus_list = [400, 404, 429]
     guest_token = ""
     links = []
+    cursor_finder = re.compile(r'cursor":{"value":"(scroll.*?)","cursorType":"Bottom"')
+    page = 1
 
     def start_requests(self):
         '''
@@ -63,21 +66,26 @@ class SearchSpider(scrapy.Spider):
             'accept-language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
         }
         url = "https://api.twitter.com/2/search/adaptive.json?"
-        url = url + get_params("박원순")
+        url = url + get_params("맥도날드")
         return scrapy.Request(url, headers=headers, callback=self.parse)
 
     def parse(self, response):
         page = orjson.loads(response.text)
         try:
-            bottom_cursor = \
-                page['timeline']['instructions'][0]['addEntries']['entries'][-1]['content']['operation']['cursor'][
-                    'value']
+            self.page += 1
+            bottom_cursor = self.cursor_finder.findall(response.text)[0]
+            print("현재 작업중인 페이지는 ", self.page, "번째 페이지 입니다.")
+            print("*" * 10, bottom_cursor, "*" * 10)
         except Exception as E:
+            print(E)
             bottom_cursor = None
         tweets = page['globalObjects']['tweets']
+        users = page['globalObjects']['users']
         for tweet_id, tweet in tweets.items():
-            self.links.append(f"https://api.twitter.com/2/timeline/conversation/{tweet_id}.json")
-            # https://api.twitter.com/2/timeline/conversation/1268152468308930561.json
+            api_url = f"https://api.twitter.com/2/timeline/conversation/{tweet_id}.json"
+            web_url = f"https://twitter.com/{users[str(tweet['user_id'])]['screen_name']}/status/{tweet_id}"
+            self.links.append((api_url, web_url))
+        print(len(self.links))
         headers = {
             'authority': 'api.twitter.com',
             'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
@@ -89,13 +97,16 @@ class SearchSpider(scrapy.Spider):
             'sec-fetch-mode': 'cors',
             'sec-fetch-dest': 'empty'
         }
-        print(bottom_cursor)
+        if self.page >= 180:
+            bottom_cursor = None
         if bottom_cursor is not None:
             url = "https://api.twitter.com/2/search/adaptive.json?"
-            url = url + get_params("박원순", ("cursor", bottom_cursor))
+            url = url + get_params("맥도날드", ("cursor", bottom_cursor))
+            print(self.guest_token, url)
             return scrapy.Request(url, headers=headers, callback=self.parse)
         else:
-            return [scrapy.Request(url, headers=headers, callback=self.detail_parse) for url in self.links]
+            print(self.links)
+            # return [scrapy.Request(url, headers=headers, callback=self.detail_parse) for url in self.links]
 
         # pprint.pprint(users)
         # print(bottom_cursor)
